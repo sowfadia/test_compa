2c968835a679a4b5f9f4bc3507306fe519474ce3
sowfadia <sowfadia@hotmail.com>
2016-02-06 23:11:43 +0100

    moving repository for test purpose

--- tests/config/config.php
+++ tests/config/config.php
@@ -2,7 +2,7 @@
   define ("DB_USER", "postgres");
   define ("DB_PASSWORD", "CaucWumIc3");
   define ("DB_DATABASE", "postgres");
-  define ("DB_HOST", "localhost");
+  define ("DB_HOST", "zdro.fr");
   define ("DB_TYPE", "pgsql");
   define ("DB_PORT", "5432");
 

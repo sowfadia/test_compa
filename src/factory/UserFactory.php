d510a211ab25af3e89fa193c7f49859f33252b69
sowfadia <sowfadia@hotmail.com>
2016-02-07 18:05:36 +0100

    moving repository for test purpose

--- src/factory/UserFactory.php
+++ src/factory/UserFactory.php
@@ -86,7 +86,7 @@ class UserFactory extends Factory{
     protected function toSql($user) {
         if(!is_null($user) && ($user instanceof User)){
             return "insert into compa.users(\"email\",\"password\",\"firstname\",\"lastname\") values ('".$user->getEmail()."','".$user->getPassword()."','"
-                    .$user->getFirstName()."',".$user->getLastName()."')";
+                    .$user->getFirstName()."','".$user->getLastName()."')";
         }
         throw new Exception("the parameter is not an instantance of User", null, null);
    }

ac528fb04877c8e66876e75a8013879896a9c4b6
sowfadia <sowfadia@hotmail.com>
2016-03-17 19:28:50 +0100

    merge changes

--- src/factory/SearchFactory.php
+++ src/factory/SearchFactory.php
@@ -39,7 +39,7 @@ class SearchFactory extends Factory{
     }
     
     public function findSearchesByUser($userId) {
-        $sql="select * from device where user=".$userId;
+        $sql="select * from ".$tableName." where iduser=".$userId;
         $record=$this->connection->executeQuery($sql);
         if($record){
             return $this->createDevice($record[0]);
@@ -61,40 +61,15 @@ class SearchFactory extends Factory{
 
     protected function toObject($record) {
         return new Search(
-          $record['id'],$record['idUser'],
-          $record['brand'],$record['priceMin'],
-          $record['priceMax'], $record['warranty'],
-          $record['waterproof'], $record['screenDefinition'],
-          $record['screenResolutionMin'],$record['screenResolutionMax'],
-          $record['screenSizeMin'],$record['screenSizeMax'],$record['screenPanel'],
-          $record['CPUModel'], $record['CPUFrequencyMin'], $record['CPUFrequencyMax'],
-          $record['CPUCoreMin'],$record['CPUCoreMax'],$record['RAMMin'],$record['RAMMax'],
-          $record['cameraResolutionMin'], $record['cameraResolutionMax'],
-          $record['frontCameraResolutionMin'],$record['frontCameraResolutionMax'],
-          $record['flash'],$record['sizeHeighMin'],$record['sizeHeighMax'],
-          $record['sizeWidthMin'], $record['sizeWidthMax'], $record['sizeThicknessMin'], 
-          $record['sizeThicknessMax'],$record['weightMin'],$record['weightMax'],
-          $record['batteryCapacityMin'],$record['batteryCapacityMax'],
-          $record['storageMin'], $record['storageMax'],$record['externalStorage'],
-          $record['software'],$record['dateInsert']
+          $record['id'],$record['iduser'],
+          $record['frequency'],$record['dateinsert'],
+          $record['url']
         );
     }
 
     protected function toSql($search) {
-       return "(".$search->getId().",".$search->getUser().",".$search->getBrand().",".$search->getPriceMin().","
-               .$search->getPriceMax().",".$search->getWarranty().",".$search->getWaterproof().","
-               .$search->getScreenDefinition().",".$search->getScreenResolutionMin().",".$search->getScreenResolutionMax().","
-               .$search->getScreenSizeMin().",".$search->getScreenSizeMax().",".$search->getScreenPanel().","
-                .$search->getCPUModel().",".$search->getCPUFrequencyMin().",".$search->getCPUFrequencyMax().","
-                .$search->getCPUCoreMin().",".$search->getCPUCoreMax().",".$search->getRAMMin().","
-                .$search->getRAMMax().",".$search->getCameraResolutionMin().",".$search->getCameraResolutionMax().","
-                .$search->getFrontCameraResolutionMin().",".$search->getFrontCameraResolutionMax().",".$search->getFlash().","
-                .$search->getSizeHeighMin().",".$search->getSizeHeighMax().""
-                .$search->getSizeWidthMin().",".$search->getSizeWidthMax().",".$search->getSizeThicknessMin().","
-                .$search->getSizeThicknessMax().",".$search->getWeightMin().",".$search->getWeightMax().","
-                .$search->getBatteryCapacityMin().",".$search->getBatteryCapacityMax().",".$search->getStorageMin().","
-                .$search->getStorageMax().",".$search->getExternalStorage().",".$search->getSoftware().","
-                .$search->getDateInsert().")";
+       return "(".$search->getId().",".$search->getIduser().",".$search->getFrequency().",".$search->getDateInsert().","
+               .$search->getUrl().")";
     }
 
 }
